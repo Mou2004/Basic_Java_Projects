cc4760206c0030014464b3cf0fedd02253eaf1a3
Mou2004 <92316657+Mou2004@users.noreply.github.com>
2025-03-12 20:14:36 +0100

    initial version-logic has some flaw-working on it

--- TicTacToe/TicTacToe.java
+++ TicTacToe/TicTacToe.java
@@ -16,36 +16,36 @@ public class TicTacToe {
 
 
         var numberOfSquaresPlayed = 0;
-        var whoseTurnItIs ='x';
+        var player ='x';
 
 
         while(numberOfSquaresPlayed<= 9){
             printTheBoard(board);
-            System.out.printf("choose a square player %s", whoseTurnItIs, ": ");
+            System.out.printf("choose a square player %s", player, ": ");
             Scanner scanner = new Scanner(System.in);
             var input = scanner.nextInt();
-            board[input-1] = whoseTurnItIs;
+            board[input-1] = player;
             if(
-                                (board[0]+board[1]+board[2])==(whoseTurnItIs*3)
-                            || (board[3] + board[4] + board[5] == (whoseTurnItIs * 3)) // second row
-                            || (board[6] + board[7] + board[8] == (whoseTurnItIs * 3)) // third row
-                            || (board[0] + board[3] + board[6] == (whoseTurnItIs * 3)) // first column
-                            || (board[1] + board[4] + board[7] == (whoseTurnItIs * 3)) // second column
-                            || (board[2] + board[5] + board[8] == (whoseTurnItIs * 3)) // third column
-                            || (board[0] + board[4] + board[8] == (whoseTurnItIs * 3)) // first diagonal
-                            || (board[2] + board[4] + board[6] == (whoseTurnItIs * 3)) // second diagonal
+                                (board[0]+board[1]+board[2])==(player*3)
+                            || (board[3] + board[4] + board[5] == (player * 3)) // second row
+                            || (board[6] + board[7] + board[8] == (player * 3)) // third row
+                            || (board[0] + board[3] + board[6] == (player * 3)) // first column
+                            || (board[1] + board[4] + board[7] == (player * 3)) // second column
+                            || (board[2] + board[5] + board[8] == (player * 3)) // third column
+                            || (board[0] + board[4] + board[8] == (player * 3)) // first diagonal
+                            || (board[2] + board[4] + board[6] == (player * 3)) // second diagonal
 
             )
             {    printTheBoard(board);
-                System.out.printf("you won!! congratulations %s", whoseTurnItIs);
+                System.out.printf("you won!! congratulations %s", player);
                 break;
             }
 
-            if (whoseTurnItIs=='x'){
-                    whoseTurnItIs ='o';
+            if (player=='x'){
+                    player ='o';
                 }
             else {
-                    whoseTurnItIs ='x';
+                    player ='x';
                 }
 
 
