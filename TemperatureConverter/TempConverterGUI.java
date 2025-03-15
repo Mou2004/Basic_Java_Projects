8d67398b3836820a9d409ecc66a836cde1f6fe37
Mou2004 <92316657+Mou2004@users.noreply.github.com>
2025-03-15 12:15:43 +0100

    basic layout for TempConverter GUI

---
+++ TemperatureConverter/TempConverterGUI.java
@@ -0,0 +1,66 @@
+package TemperatureConverter;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class TempConverterGUI {
+    //For the GUI version, use JTextField for input and JLabel for the result.
+        private JFrame frame;
+        private JTextField textField;
+        private JLabel label;
+        public TempConverterGUI(){
+            this.frame = new JFrame();
+            frame.setLayout(new GridBagLayout());
+            frame.setSize(600,600);
+            frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+            frame.setVisible(true);
+
+            //create gridBag constraints
+            GridBagConstraints gbc = new GridBagConstraints();
+            gbc.insets = new Insets(10,10,10,10);//adding padding between components
+
+            //create label and add it to the top
+            this.label = new JLabel("Enter temperature...");
+            gbc.gridx =0; //Column 0
+            gbc.gridy=0; //row 0
+            gbc.gridwidth=2;//span two columns
+            frame.add(label, gbc);
+
+            this.textField = new JTextField(20);//20 columns wide
+            gbc.gridx =0; //Column 0
+            gbc.gridy=1; //row 0
+            gbc.gridwidth=2;//span two columns
+            textField.setText("Hello World");
+            frame.add(textField, gbc);
+
+            JButton CtoFButton = new JButton("Convert C to F");
+            gbc.gridx =0; //Column 0
+            gbc.gridy=3; //row 0
+            gbc.gridwidth=1;//span one column
+            CtoFButton.addActionListener(new ButtonClickListener());
+            frame.add(CtoFButton, gbc);
+
+            JButton FtoCButton = new JButton("Convert F to C");
+            gbc.gridx =2; //Column 0
+            gbc.gridy=3; //row 0
+            gbc.gridwidth=1;//span one column
+            frame.add(FtoCButton, gbc);
+
+
+        }
+
+    public static void main(String[] args) {
+        new TempConverterGUI();
+    }
+
+    public class ButtonClickListener implements ActionListener{
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+
+        }
+    }
+}
---
+++ TicTacToe/production/Basic_Java_Projects/production/Basic_Java_Projects/README.md
@@ -0,0 +1,5 @@
+# Basic Java Projects for beginners  
+These are the small projects I did in my first semester of Computer science while learning Java
+## Calculator-  
+
+![Calculator screenshot](https://github.com/Mou2004/Basic_Java_Projects/blob/main/calculator.PNG)
