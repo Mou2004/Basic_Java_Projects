e8f63d9788733885140f875ae2d3e235fde28804
Mou2004 <92316657+Mou2004@users.noreply.github.com>
2025-03-15 16:38:17 +0100

    Event handling logic implemented for buttons

--- TemperatureConverter/TempConverterGUI.java
+++ TemperatureConverter/TempConverterGUI.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 
 public class TempConverterGUI {
     //For the GUI version, use JTextField for input and JLabel for the result.
+    //remaining-- implement logic to handle invalid user input in textfield- try catch in action perfomed when defining temp float
         private JFrame frame;
         private JTextField textField;
         private JLabel label;
@@ -41,12 +42,16 @@ public class TempConverterGUI {
             gbc.gridy=3; //row 0
             gbc.gridwidth=1;//span one column
             CtoFButton.addActionListener(new ButtonClickListener());
+            CtoFButton.setActionCommand("CtoF");
             frame.add(CtoFButton, gbc);
 
             JButton FtoCButton = new JButton("Convert F to C");
             gbc.gridx =2; //Column 0
             gbc.gridy=3; //row 0
             gbc.gridwidth=1;//span one column
+
+            FtoCButton.addActionListener(new ButtonClickListener());
+            FtoCButton.setActionCommand("FtoC");
             frame.add(FtoCButton, gbc);
 
 
@@ -60,7 +65,15 @@ public class TempConverterGUI {
 
         @Override
         public void actionPerformed(ActionEvent e) {
-
+            String command = e.getActionCommand();
+            Float temperature = Float.parseFloat(textField.getText());
+            if (command.equals("CtoF")){
+                float resultinF = (float) ((float) (temperature*(9.0)/5.0) + 32.0);
+                textField.setText(String.valueOf(resultinF));
+            } else if (command.equals("FtoC")) {
+                float resultinC = (float) ((float) ((temperature-32)*(5.0)/9.0) );
+                textField.setText(String.valueOf(resultinC));
+            }
         }
     }
 }
